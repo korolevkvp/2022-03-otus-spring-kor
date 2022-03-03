c214c058b020efc2aafb36b8612d5e1913161388
Вадим Королев <vkorolev@diasoft.ru>
2022-03-03 16:45:08 +0300

    чтение ресурса

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -2,18 +2,21 @@ package ru.otus.spring;
 
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: создайте здесь класс контекста
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        // TODO: Получите Person Service
-        PersonService service = context.getBean(PersonService.class);
-        // Получите Person "Ivan"
-        Person ivan = service.getByName("Ivan");
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+        Resource questions = context.getResource("/questions.csv");
+        try {
+            System.out.println(questions.contentLength());
+        } catch (IOException e) {
+            System.out.println("Файл " + questions.getFilename() + " не найден.");
+        }
     }
 }
