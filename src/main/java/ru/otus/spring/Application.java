ba532b8bd719a51477c15f8e4f5b8dbc6a79f687
Вадим Королев <vkorolev@diasoft.ru>
2022-03-18 20:38:42 +0300

    Spring Shell. Перенос опроса в команду. Удаление опроса из основного потока.

--- src/main/java/ru/otus/spring/Application.java
+++ src/main/java/ru/otus/spring/Application.java
@@ -2,21 +2,12 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.otus.spring.domain.Question;
-import ru.otus.spring.service.QuestionService;
-
-import java.io.IOException;
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		QuestionService service = context.getBean(QuestionService.class);
-		List<Question> questions = service.getQuestions();
-		service.startQuiz(questions);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
