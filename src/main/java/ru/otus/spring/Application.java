432b543b4d15c5200ec9011465be3810963ac084
Вадим Королев <vkorolev@diasoft.ru>
2022-03-21 18:21:48 +0300

    Создание структуры таблицы с помощью liquibase.

--- src/main/java/ru/otus/spring/Application.java
+++ src/main/java/ru/otus/spring/Application.java
@@ -1,15 +1,20 @@
 package ru.otus.spring;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.sql.SQLException;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
         SpringApplication.run(Application.class, args);
+        Console.main(args);
     }
 
 }
