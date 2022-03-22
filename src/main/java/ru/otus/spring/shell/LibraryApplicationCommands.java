0e2ef8d41f09e71b5f76ded91957dbdfb202d820
Вадим Королев <vkorolev@diasoft.ru>
2022-03-22 17:59:28 +0300

    shell delete

--- src/main/java/ru/otus/spring/shell/LibraryApplicationCommands.java
+++ src/main/java/ru/otus/spring/shell/LibraryApplicationCommands.java
@@ -21,10 +21,20 @@ public class LibraryApplicationCommands {
         return libraryService.findAllBooks();
     }
 
-    @ShellMethod(value = "Update book by id", key = {"ub", "update_book"})
+    @ShellMethod(value = "Update book by id (example: 3 Buratino,8,4,1)", key = {"ub", "update_book"})
     public Book updateBookById(Long id, BookCreate bookCreate) throws Exception {
         try {
             return libraryService.updateBookById(id, bookCreate);
+        } catch (Exception e) {
+            throw new SQLException("There is no book with id = " + id);
+        }
+    }
+
+    @ShellMethod(value = "Delete book by id", key = {"db", "delete_book"})
+    public void deleteBookById(Long id) throws Exception {
+        try {
+            libraryService.deleteBookById(id);
+            System.out.println("Book with id = " + id +" successfully deleted");
         } catch (Exception e) {
             throw new SQLException("There is no book with id = " + id);
         }
