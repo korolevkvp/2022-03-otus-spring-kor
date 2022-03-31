b38dfb3f4681eb6d39735339fdf459acad660a11
Вадим Королев <vkorolev@diasoft.ru>
2022-03-31 17:29:22 +0300

    начало описания BookRepositoryJpa

--- src/main/java/ru/otus/spring/repository/BookRepositoryJpa.java
+++ src/main/java/ru/otus/spring/repository/BookRepositoryJpa.java
@@ -1,4 +1,17 @@
 package ru.otus.spring.repository;
 
+import ru.otus.spring.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepositoryJpa {
+    Book save(Book book);
+    Optional<Book> findById(long id);
+
+    List<Book> findAll();
+    List<Book> findByName(String name);
+
+    void updateNameById(long id, String name);
+    void deleteById(long id);
 }
--- src/main/java/ru/otus/spring/repository/BookRepositoryJpaImpl.java
+++ src/main/java/ru/otus/spring/repository/BookRepositoryJpaImpl.java
@@ -1,4 +1,51 @@
 package ru.otus.spring.repository;
 
+import org.springframework.stereotype.Repository;
+import ru.otus.spring.domain.Book;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class BookRepositoryJpaImpl implements BookRepositoryJpa {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Book save(Book book) {
+        if (book.getId() == null) {
+            em.persist(book);
+            return book;
+        }
+        return em.merge(book);
+    }
+
+    @Override
+    public Optional<Book> findById(long id) {
+        return Optional.ofNullable(em.find(Book.class, id));
+    }
+
+    @Override
+    public List<Book> findAll() {
+        EntityGraph<?> entityGraph = em.getEntityGraph("")
+        TypedQuery<Book> query = em.createQuery("select b from Book b ", Book.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Book> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void updateNameById(long id, String name) {
+
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
 }
