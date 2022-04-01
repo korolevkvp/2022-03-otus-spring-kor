3cb6c0ecd4853f9820e5870fdc73f1fbc1107b88
Вадим Королев <vkorolev@diasoft.ru>
2022-04-01 17:02:33 +0300

    comment репозиторий

--- src/main/java/ru/otus/spring/repository/CommentRepositoryJpa.java
+++ src/main/java/ru/otus/spring/repository/CommentRepositoryJpa.java
@@ -1,4 +1,14 @@
 package ru.otus.spring.repository;
 
+import ru.otus.spring.domain.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepositoryJpa {
+    Comment save(Comment comment);
+    Optional<Comment> findById(long id);
+
+    List<Comment> findAll();
+    List<Comment> findByName(String name);
 }
--- src/main/java/ru/otus/spring/repository/CommentRepositoryJpaImpl.java
+++ src/main/java/ru/otus/spring/repository/CommentRepositoryJpaImpl.java
@@ -1,4 +1,48 @@
 package ru.otus.spring.repository;
 
+import org.springframework.stereotype.Repository;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.Comment;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class CommentRepositoryJpaImpl implements CommentRepositoryJpa {
+
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() == null) {
+            em.persist(comment);
+            return comment;
+        }
+        return em.merge(comment);
+    }
+
+    @Override
+    public Optional<Comment> findById(long id) {
+        return Optional.ofNullable(em.find(Comment.class, id));
+    }
+
+    @Override
+    public List<Comment> findAll() {
+        return em.createQuery("select c from Comment c ", Comment.class).getResultList();
+    }
+
+    @Override
+    public List<Comment> findByName(String name) {
+        TypedQuery<Comment> query = em.createQuery("select c " +
+                        "from Comment c " +
+                        "where c.name = :name",
+                Comment.class);
+        query.setParameter("name", name);
+        return query.getResultList();
+    }
 }
