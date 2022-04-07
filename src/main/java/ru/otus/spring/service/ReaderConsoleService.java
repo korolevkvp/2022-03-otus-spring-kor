752390af9fbd51491dc9425fadbbe6b82ba941e6
Вадим Королев <vkorolev@diasoft.ru>
2022-04-07 16:41:44 +0300

    Тесты для репозитория книг.

--- src/main/java/ru/otus/spring/service/ReaderConsoleService.java
+++ src/main/java/ru/otus/spring/service/ReaderConsoleService.java
@@ -18,13 +18,13 @@ public class ReaderConsoleService implements ReaderService{
     @Override
     public Book readBook() {
         Book book = new Book();
-        System.out.println("\nВведите название книги (от 1 до 10)\n$ ");
+        System.out.print("\nВведите название книги \n$ ");
         book.setTitle(scanner.nextLine());
-        System.out.println("\nВведите рейтинг книги (от 1 до 10)\n$ ");
+        System.out.print("\nВведите рейтинг книги (от 1 до 10)\n$ ");
         book.setRating(scanner.nextInt());
         book.setAuthor(readAuthor());
         book.setGenre(readGenre());
-        System.out.println("\nВведите количество комментариев\n$ ");
+        System.out.print("\nВведите количество комментариев\n$ ");
         int commentCount = scanner.nextInt();
         List<Comment> comments = new LinkedList<>();
         for (int i = 0; i < commentCount; ++i) {
@@ -37,9 +37,10 @@ public class ReaderConsoleService implements ReaderService{
     @Override
     public Author readAuthor() {
         Author author = new Author();
-        System.out.println("\nВведите имя автора\n$ ");
+        System.out.print("\nВведите имя автора\n$ ");
+        scanner.nextLine();
         author.setName(scanner.nextLine());
-        System.out.println("\nВведите фамилию автора\n$ ");
+        System.out.print("\nВведите фамилию автора\n$ ");
         author.setSurname(scanner.nextLine());
         return author;
     }
@@ -47,7 +48,7 @@ public class ReaderConsoleService implements ReaderService{
     @Override
     public Genre readGenre() {
         Genre genre = new Genre();
-        System.out.println("\nВведите название жанра\n$ ");
+        System.out.print("\nВведите название жанра\n$ ");
         genre.setName(scanner.nextLine());
         return genre;
     }
@@ -56,6 +57,7 @@ public class ReaderConsoleService implements ReaderService{
     public Comment readComment() {
         Comment сomment = new Comment();
         System.out.println("\nВведите автора комментария\n$ ");
+        scanner.nextLine();
         сomment.setAuthor(scanner.nextLine());
         System.out.println("\nВведите содержимое комментария\n$ ");
         сomment.setContent(scanner.nextLine());
