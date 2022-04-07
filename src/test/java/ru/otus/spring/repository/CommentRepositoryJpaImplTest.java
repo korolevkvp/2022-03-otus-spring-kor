015742804bfe6094300fe7d74418081594c139b6
Вадим Королев <vkorolev@diasoft.ru>
2022-04-07 16:48:35 +0300

    Тесты для всех репозиториев.

---
+++ src/test/java/ru/otus/spring/repository/CommentRepositoryJpaImplTest.java
@@ -0,0 +1,70 @@
+package ru.otus.spring.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.spring.domain.Comment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jpa для работы с комментариями")
+@DataJpaTest
+@Import(CommentRepositoryJpaImpl.class)
+class CommentRepositoryJpaImplTest {
+
+    @Autowired
+    private CommentRepositoryJpaImpl repositoryJpa;
+
+    @DisplayName("должен получать список комментариев по автору")
+    @Test
+    void findByAuthor() {
+        Comment comment = repositoryJpa.save(comment());
+
+        assertThat(repositoryJpa.findByAuthor(comment.getAuthor())).usingElementComparatorIgnoringFields("id").contains(comment);
+    }
+
+    @DisplayName("должен корректно выполнять сохранение комментария")
+    @Test
+    void shouldCorrectSaveComment() {
+        Comment comment = repositoryJpa.save(comment());
+
+        assertThat(comment.getId()).isNotNull();
+        assertThat(repositoryJpa.findAll()).usingElementComparatorIgnoringFields("id").contains(comment());
+    }
+
+    @DisplayName("должен корректно получать комментарий по идентификатору")
+    @Test
+    void shouldCorrectFindCommentById() {
+        Comment comment = repositoryJpa.save(comment());
+
+        Comment testComment = comment();
+        testComment.setId(comment.getId());
+        assertThat(repositoryJpa.findById(comment.getId()).get()).isEqualTo(testComment);
+    }
+
+    @DisplayName("должен корректно получать список комментариев")
+    @Test
+    void shouldCorrectFindAllComments() {
+        repositoryJpa.save(comment());
+
+        assertThat(repositoryJpa.findAll()).isNotNull().usingElementComparatorIgnoringFields("id").contains(comment());
+    }
+
+    @DisplayName("должен корректно удалять комментарий по идентификатору")
+    @Test
+    void shouldCorrectDeleteCommentById() {
+        Comment comment = repositoryJpa.save(comment());
+
+        repositoryJpa.deleteById(comment.getId());
+
+        assertThat(repositoryJpa.findAll()).doesNotContain(comment());
+    }
+
+    private Comment comment() {
+        return new Comment(4L, "Buratino", "Great!");
+    }
+
+    
+}
---
+++ src/test/java/ru/otus/spring/repository/GenreRepositoryJpaImplTest.java
@@ -0,0 +1,70 @@
+package ru.otus.spring.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.spring.domain.Genre;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DisplayName("Репозиторий на основе Jpa для работы с книгами")
+@DataJpaTest
+@Import(GenreRepositoryJpaImpl.class)
+class GenreRepositoryJpaImplTest {
+
+    @Autowired
+    private GenreRepositoryJpaImpl repositoryJpa;
+
+    @DisplayName("должен получать список жанров по названию")
+    @Test
+    void findByName() {
+        Genre genre = repositoryJpa.save(genre());
+
+        assertThat(repositoryJpa.findByName(genre.getName())).usingElementComparatorIgnoringFields("id").contains(genre);
+    }
+
+    @DisplayName("должен корректно выполнять сохранение жанра")
+    @Test
+    void shouldCorrectSaveGenre() {
+        Genre genre = repositoryJpa.save(genre());
+
+        assertThat(genre.getId()).isNotNull();
+        assertThat(repositoryJpa.findAll()).usingElementComparatorIgnoringFields("id").contains(genre());
+    }
+
+    @DisplayName("должен корректно получать жанр по идентификатору")
+    @Test
+    void shouldCorrectFindGenreById() {
+        Genre genre = repositoryJpa.save(genre());
+
+        Genre testGenre = genre();
+        testGenre.setId(genre.getId());
+        assertThat(repositoryJpa.findById(genre.getId()).get()).isEqualTo(testGenre);
+    }
+
+    @DisplayName("должен корректно получать список жанров")
+    @Test
+    void shouldCorrectFindAllGenres() {
+        repositoryJpa.save(genre());
+
+        assertThat(repositoryJpa.findAll()).isNotNull().usingElementComparatorIgnoringFields("id").contains(genre());
+    }
+
+    @DisplayName("должен корректно удалять жанр по идентификатору")
+    @Test
+    void shouldCorrectDeleteGenreById() {
+        Genre genre = repositoryJpa.save(genre());
+
+        repositoryJpa.deleteById(genre.getId());
+
+        assertThat(repositoryJpa.findAll()).doesNotContain(genre());
+    }
+
+    private Genre genre() {
+        return new Genre(4L, "Buratino");
+    }
+
+}
