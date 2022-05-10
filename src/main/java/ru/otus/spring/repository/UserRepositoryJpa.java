1feda3892c3bd7f340eba541888f6ae8ed43ae2f
Вадим Королев <vkorolev@diasoft.ru>
2022-05-10 18:11:58 +0300

    User id -> Long !

--- src/main/java/ru/otus/spring/repository/UserRepositoryJpa.java
+++ src/main/java/ru/otus/spring/repository/UserRepositoryJpa.java
@@ -3,6 +3,6 @@ package ru.otus.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.spring.domain.User;
 
-public interface UserRepositoryJpa extends JpaRepository<User, String> {
+public interface UserRepositoryJpa extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
