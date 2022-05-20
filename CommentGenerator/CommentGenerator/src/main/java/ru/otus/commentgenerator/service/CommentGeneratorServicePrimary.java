2266543f7a18a32847e7ec484c6a3d40ebf7cae7
Вадим Королев <vkorolev@diasoft.ru>
2022-05-20 17:34:58 +0300

    Доделывание сервиса генерции комментариев

--- CommentGenerator/CommentGenerator/src/main/java/ru/otus/commentgenerator/service/CommentGeneratorServicePrimary.java
+++ CommentGenerator/CommentGenerator/src/main/java/ru/otus/commentgenerator/service/CommentGeneratorServicePrimary.java
@@ -1,7 +1,17 @@
 package ru.otus.commentgenerator.service;
 
 import org.springframework.stereotype.Service;
+import ru.otus.commentgenerator.domain.Comment;
+
+import java.util.Random;
 
 @Service
 public class CommentGeneratorServicePrimary implements CommentGeneratorService{
+    @Override
+    public Comment generate() {
+        return Comment.builder()
+                .author("Generator")
+                .content(new Random().nextBoolean() ? "Bad" : "Good")
+                .build();
+    }
 }
