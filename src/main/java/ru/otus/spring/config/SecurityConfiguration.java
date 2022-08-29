a4fc98b05e8aef414d395eb6df0fbe14ed64111f
Вадим Королев <vkorolev@diasoft.ru>
2022-08-29 13:44:51 +0300

    Аутентификация через User в бд

--- src/main/java/ru/otus/spring/config/SecurityConfiguration.java
+++ src/main/java/ru/otus/spring/config/SecurityConfiguration.java
@@ -1,12 +1,25 @@
 package ru.otus.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.otus.spring.service.UserService;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService service;
+
+    @Bean
+    public PasswordEncoder devPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -17,8 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("password").roles("ADMIN");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.service);
     }
 }
